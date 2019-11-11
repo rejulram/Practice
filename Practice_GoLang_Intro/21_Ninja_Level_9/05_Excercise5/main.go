409bdd807496aafe40b44ad19fada6cab922d1b8
Ramakrishnan,Rejul <rejulram@rediffmail.com>
2019-11-11 18:17:52 +0530

    Adding Excercises 21_Ninja/05 and 06

---
+++ Practice_GoLang_Intro/21_Ninja_Level_9/05_Excercise5/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var incrementer int64 = 0
+
+func main() {
+	var wg sync.WaitGroup
+	gs := 100
+	wg.Add(100)
+	for i:=0;i<gs;i++{
+		go func(){
+			atomic.AddInt64(&incrementer,1)
+			fmt.Println(atomic.LoadInt64(&incrementer))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Final value :",incrementer)
+}
