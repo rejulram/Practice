e6218db757fe962cdbc2b982ed668322e405feaf
Ramakrishnan,Rejul <rejulram@rediffmail.com>
2019-11-14 19:00:41 +0530

    Added web dev Hands on excercise section 41/ 07,08

--- Practice_WebDev/05_Understanding_routing/41_hands_on_exercises/08/main.go
+++ Practice_WebDev/05_Understanding_routing/41_hands_on_exercises/08/main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"strings"
 )
 
 func main() {
@@ -24,34 +23,29 @@ func main() {
 		go serve(conn)
 	}
 }
-func serve(conn net.Conn){
+
+func serve(conn net.Conn) {
 	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
-	var i int
-	var rMethod,rURI string
-	for scanner.Scan() {
+	for scanner.Scan(){
 		ln := scanner.Text()
-		//fmt.Println(ln)
-		if i==0{
-			xs := strings.Fields(ln)
-			rMethod = xs[0]
-			rURI = xs[1]
-			fmt.Println("METHOD",rMethod)
-			fmt.Println("URI",rURI)
+		if ln == ""{
+			fmt.Println("END OF HEADERS")
+			break
 		}
-		if ln == "" {
-			fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADERS")
-			break;
-		}
-		i++
 	}
-	body := "CHECKOUT RESPONSE BODY PAYLOAD"
-	body += "\n"
-	body += rMethod
-	body += "\n"
-	body += rURI
+	body := `<!DOCTYPE html>
+				<html lang="en">
+					<head>
+						<meta charset="UTF-8">
+						<title>Welecome TCP server</title>
+					</head>
+					<body>
+						<h1>HOLY COW THIS IS LOW LEVEL</h1>
+					</body>
+				</html>`
 	io.WriteString(conn,"HTTP/1.1 200 OK\r\n")
-	fmt.Fprint(conn,"Content-Type : text/plain\r\n")
+	fmt.Fprint(conn,"Content-Type : text/html\r\n")
 	fmt.Fprintf(conn,"Content-Length : %d \r\n",len(body))
 	io.WriteString(conn,"\r\n")
 	io.WriteString(conn,body)
